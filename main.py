5dabe3c380ba0b98ae4405f56815f2f0976c76ef
GonzaloLandriel <cesargonzalolandriel@gmail.com>
2025-04-21 22:47:29 +0000

    Project done

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sys
+
+from stats import count_words
+
+from stats import count_characters
+
+print(sys.argv)
+
 def get_book_text(path_to_file):
 
     with open(path_to_file) as f:
@@ -6,16 +14,21 @@ def get_book_text(path_to_file):
         return f.read()
 
 def main():
-    text= get_book_text("books/frankenstein.txt") 
+
+    path_to_file = sys.argv[1]
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text= get_book_text(sys.argv[1]) 
     
 
 main()
 
 
 
-from stats import count_words
 
-from stats import count_characters
 
 
 
--- stats.py
+++ stats.py
@@ -1,4 +1,12 @@
+import sys
+
+if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+
 def count_words(path_to_file):
+   
     with open(path_to_file) as f:
         book_as_string= f.read()
     split=book_as_string.split()        
@@ -6,7 +14,7 @@ def count_words(path_to_file):
     print (f" Found {word_count} total words")
 
 # Call the function
-count_words("books/frankenstein.txt")
+count_words(sys.argv[1])
 
 def count_characters (path_to_file):
     with open(path_to_file) as f:
@@ -22,21 +30,39 @@ def count_characters (path_to_file):
     
     return num_characters
 
-count_characters("books/frankenstein.txt")
-character_counts = count_characters("books/frankenstein.txt")
+count_characters(sys.argv[1])
+character_counts = count_characters(sys.argv[1])
 
 def make_only_alpha(non_alpha_list):
 
 # Create a new dictionary containing only alphabetic keys
     filtered_counts = {char: count for char, count in non_alpha_list.items() if char.isalpha()}
 
-    print(filtered_counts)
-    print(type(filtered_counts))
+    return filtered_counts
 # Output: {'a': 10, 'b': 5}
-make_only_alpha(character_counts)
 
 
+only_alpha_dict=make_only_alpha(character_counts)
 
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(dict):
+    return dict["num"]
 
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list_with_symbols= []
+    for ch in num_chars_dict:
+        sorted_list_with_symbols.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list_with_symbols.sort(reverse=True, key=sort_on)
+    
 
+    for item in sorted_list_with_symbols:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    
+
+    
+# [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
+chars_dict_to_sorted_list(only_alpha_dict)
 
